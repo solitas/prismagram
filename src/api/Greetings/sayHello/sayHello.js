654cb8cbe15c803b5347b0052532afb4fa5bbe1e
solitas <solitas.roh@kakao.com>
2020-03-20 21:40:29 +0900

    data model 정의 , prisma-client-lib  추가

---
+++ src/api/Greetings/sayHello/sayHello.js
@@ -0,0 +1,12 @@
+import {
+  prisma
+} from "../../../../generated/prisma-client";
+
+export default {
+  Query: {
+    sayHello: async () => {
+      console.log(await prisma.users());
+      return "HELLO";
+    }
+  }
+};
